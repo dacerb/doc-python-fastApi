68f28021bf5247381eb303216cffab200af418c0
David Acerbo <david.acerbo@mercadolibre.com>
2023-02-11 12:32:13 -0300

    update

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
+#python libs
+from typing import Optional
 
+# pydantic
+from pydantic import BaseModel
+
+#FastAPI
 from fastapi import FastAPI
+from fastapi import Body
 
 app = FastAPI()
+
+## models
+class Person(BaseModel):
+    fist_name: str
+    last_name: str
+    age: int
+    hair_color: Optional[str] = None
+    is_married: Optional[bool] = None
+
 
 
 @app.get("/")
@@ -12,5 +28,7 @@ def home():
 ## Request and Response body
 ## path operation path decoration
 @app.post("/person/new")  # Path operation decoration
-def create_person():      # Path operation function
-    return ""
+def create_person(person: Person = Body(...)):      # Path operation function
+
+
+    return person
