6b63958013cd8a3de3daea92715acbef57bf26b8
David <david.acerbo@gmail.com>
2023-02-20 16:16:03 -0300

    update

--- main.py
+++ main.py
@@ -133,12 +133,14 @@ def show_person(
             max_length=50,
             regex="^[a-zA-Z]*$",
             title="Person Name",
-            description="This is the person name. It's between 1 and 50 characters"
+            description="This is the person name. It's between 1 and 50 characters",
+            example="martin"
         ),
         age: int = Query(
             ...,
             title="Person Age",
-            description="This is the person age. It's required"
+            description="This is the person age. It's required",
+            example=23
         )
 ):
     return {
@@ -153,8 +155,8 @@ def show_person(
         ...,
         gt=0,
         title="Person ID",
-        description="This is the person ID. this valeu greater That 0"
-
+        description="This is the person ID. this valeu greater That 0",
+        example=123
     )
 ):
     return {
@@ -172,7 +174,8 @@ def update_person(
         ...,
         title="Person ID",
         description="This is the person ID",
-        gt=0
+        gt=0,
+        example=124
     ),
     person: Person = Body(
         ...,
