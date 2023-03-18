bcb64c00742facb57273ba0652c76f9c393f0889
dacerbo <david.acerbo@mercadolibre.com>
2023-03-18 13:23:18 -0300

    update

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ from fastapi import Body, Query, Path
 
 app = FastAPI()
 
-
 class HairColor(Enum):
     white = "white"
     brown = "brown"
@@ -27,7 +26,6 @@ class Person(BaseModel):
     age: int
     hair_color: Optional[str] = None
     is_married: Optional[bool] = None
-
 
 class PersonTwo(BaseModel):
     fist_name: str = Field(
@@ -78,10 +76,47 @@ class PersonThree(BaseModel):
             }
         }
 
-
-
 # cargando los ejemplos en la definicion
 class PersonFour(BaseModel):
+    fist_name: str = Field(
+        ...,
+        min_length=1,
+        max_length=50,
+        example=""
+    )
+    last_name: str = Field(
+        ...,
+        min_length=1,
+        max_length=50,
+        example=""
+    )
+    age: int = Field(
+        ...,
+        gt=0,
+        le=115,
+        example=66
+    )
+    hair_color: Optional[HairColor] = Field(default=None, example="red")
+    is_married: Optional[bool] = Field(default=None, example=False)
+    password: str = Field(..., min_length=8)
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "fist_name": "dav_cfg",
+                "last_name": "XS",
+                "age": 21,
+                "hair_color": "white",
+                "is_married": False,
+                "password": "falso 123"
+            }
+        }
+
+
+class PersonFourOut(BaseModel):
+    """
+    Clase definida para output para quietar la pass asi no mostramos ese dato
+    """
     fist_name: str = Field(
         ...,
         min_length=1,
@@ -104,12 +139,50 @@ class PersonFour(BaseModel):
     is_married: Optional[bool] = Field(default=None, example=False)
 
 
+class PersonFix(BaseModel):
+    fist_name: str = Field(
+        ...,
+        min_length=1,
+        max_length=50,
+        example=""
+    )
+    last_name: str = Field(
+        ...,
+        min_length=1,
+        max_length=50,
+        example=""
+    )
+    age: int = Field(
+        ...,
+        gt=0,
+        le=115,
+        example=66
+    )
+    hair_color: Optional[HairColor] = Field(default=None, example="red")
+    is_married: Optional[bool] = Field(default=None, example=False)
 
+class PersonFourFix(PersonFix):
+    password: str = Field(..., min_length=8)
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "fist_name": "dav_cfg",
+                "last_name": "XS",
+                "age": 21,
+                "hair_color": "white",
+                "is_married": False,
+                "password": "falso 123"
+            }
+        }
+class PersonFourOutFix(PersonFix):
+    pass
 
 class Locations(BaseModel):
     city: str
     state: str
     country: str
+
 
 @app.get("/")
 def home():
@@ -120,6 +193,14 @@ def home():
 ## path operation path decoration
 @app.post("/person/new")  # Path operation decoration
 def create_person(person: Person = Body(...)):      # Path operation function
+    return person
+
+@app.post("/person/new/four", response_model=PersonFourOut)  # Path operation decoration
+def create_person(person: PersonFour = Body(...)):      # Path operation function
+    return person
+
+@app.post("/person/new/four/fix", response_model=PersonFourOutFix)  # Path operation decoration
+def create_person(person: PersonFourFix = Body(...)):      # Path operation function
     return person
 
 
